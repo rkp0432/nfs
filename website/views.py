8ccdd27046bbbf399e36f422811c8380d88c5b9a
Ram Kumar <40028854+rkp0432@users.noreply.github.com>
2023-08-08 17:13:02 +0530

    Update views.py

--- website/views.py
+++ website/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from django.views.decorators.csrf import csrf_exempt
 def index(request):
 	return render(request, 'website/index.html')
 
